7e5841c80ee3b251a54c0cc7a37287d46ac409e0
root <guibin1989@126.com>
2015-05-07 14:52:00 +0800

    Signed-off-by: root <guibin1989@126.com>

---
+++ hostlist/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('hostlist.views',
+    # Examples:
+    # url(r'^$', 'oms.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^list/$', 'host_list', name='host_list'),
+    url(r'^add/$', 'host_list_manage', name='host_add'),
+    url(r'^delete/$', 'host_list_manage', name='host_delete'),
+    url(r'^manage/(?P<id>\d+)/$', 'host_list_manage', name='host_manage'),
+)
