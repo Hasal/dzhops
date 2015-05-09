3b0c52d45f3fa259a46f5e9e8c06d54eb76ec5bd
Guibin <guibin1989@126.com>
2015-05-10 00:56:04 +0800

    Signed-off-by: Guibin <guibin1989@126.com>

--- common/views.py
+++ common/views.py
@@ -60,33 +60,31 @@ def module_deploy(request):
 
     ret = [] 
     jid = []
-    #user = 'zhaogb'
     if request.method == 'POST':
         action = request.get_full_path().split('=')[1]
         if action == 'deploy':
             tgt = request.POST.get('tgt')
             arg = request.POST.getlist('module')
-            #tgtcheck = HostList.objects.filter(hostname=tgt)
-        if True:
-            sapi = SaltAPI(url=settings.SALT_API['url'],username=settings.SALT_API['user'],password=settings.SALT_API['password'])  
-            #sapi = SaltAPI(url='http://127.0.0.1:18000/',username='zhaogb',password='dzhinternet')
-            for i in arg:
-                obj = sapi.async_deploy(tgt,i)
-                jid.append(obj)
-            db = db_operate()
-            for i in jid:
-                time.sleep(10)
-                sql = 'select full_ret from salt_returns where jid=%s'
-                result = db.select_table(settings.RETURNS_MYSQL,sql,str(i))    #通过jid获取执行结果        
-                ret.append(result)
+        if tgt:
+            if arg:
+                sapi = SaltAPI(url=settings.SALT_API['url'],username=settings.SALT_API['user'],password=settings.SALT_API['password'])  
+                for i in arg:
+                    obj = sapi.async_deploy(tgt,i)
+                    jid.append(obj)
+                db = db_operate()
+                for i in jid:
+                    time.sleep(10)
+                    sql = 'select `return` from salt_returns where jid=%s'
+                    result = db.select_table(settings.RETURNS_MYSQL,sql,str(i))    #通过jid获取执行结果        
+                    ret.append(result)
+            else:
+                ret = '请选择将要部署的模块！'
         else:
-           ret = '亲，目标主机不对，请重新输入'   
+           ret = '亲，没有指定目标主机，请重新输入！'   
 
     return render_to_response('salt_module_deploy.html', 
            {'ret': ret},context_instance=RequestContext(request)) 
 
-#--------------------------------------------begin--------------------------------------------------------
-#---------------------------------------------------------------------------------------------------------
 def module_update(request):
     """
     update (mobile/class/prog..etc) module
@@ -94,32 +92,31 @@ def module_update(request):
 
     ret = [] 
     jid = []
-    #user = 'zhaogb'
     if request.method == 'POST':
         action = request.get_full_path().split('=')[1]
         if action == 'deploy':
             tgt = request.POST.get('tgt')
             arg = request.POST.getlist('module')
-            tgtcheck = HostList.objects.filter(hostname=tgt)
-        if True:
-            sapi = SaltAPI(url=settings.SALT_API['url'],username=settings.SALT_API['user'],password=settings.SALT_API['password'])  
-            #sapi = SaltAPI(url='http://127.0.0.1:18000/',username='zhaogb',password='dzhinternet')
-            for i in arg:
-                obj = sapi.async_deploy(tgt,i)
-                jid.append(obj)
-            db = db_operate()
-            for i in jid:
-                time.sleep(30)
-                sql = 'select `return` from salt_returns where jid=%s'
-                result = db.select_table(settings.RETURNS_MYSQL,sql,str(i))    #通过jid获取执行结果
-                ret.append(result)
+        if tgt:
+            if arg:
+                sapi = SaltAPI(url=settings.SALT_API['url'],username=settings.SALT_API['user'],password=settings.SALT_API['password'])  
+                for i in arg:
+                    obj = sapi.async_deploy(tgt,i)
+                    jid.append(obj)
+                db = db_operate()
+                for i in jid:
+                    time.sleep(30)
+                    sql = 'select `return` from salt_returns where jid=%s'
+                    result = db.select_table(settings.RETURNS_MYSQL,sql,str(i))    #通过jid获取执行结果
+                    ret.append(result)
+            else:
+                ret = '请选择将要更新的模块！'
         else:
-           ret = '亲，目标主机不对，请重新输入'   
+           ret = '亲，没有指定目标主机，请重新输入！'   
 
     return render_to_response('salt_module_update.html', 
            {'ret': ret},context_instance=RequestContext(request)) 
-#----------------------------------------------end--------------------------------------------------------
-#---------------------------------------------------------------------------------------------------------
+
 def remote_execution(request):
     """
     remote command execution
@@ -128,29 +125,28 @@ def remote_execution(request):
     ret = ''
     tret = ''
     dangerCmd = ('rm','reboot','init','shutdown','poweroff')
-    #user = request.user
     if request.method == 'POST':
         action = request.get_full_path().split('=')[1]
         if action == 'exec':
             tgt = request.POST.get('tgt')
             arg = request.POST.get('arg')    
-            if tgt:
-                if arg:
-                    argCmd = arg.split()[0]
-                    argCheck = argCmd not in dangerCmd
-                    if argCheck:
-                        sapi = SaltAPI(url=settings.SALT_API['url'],username=settings.SALT_API['user'],password=settings.SALT_API['password'])
-                        unret = sapi.remote_execution(tgt,'cmd.run',arg)
-                        for kret in unret.keys():
-                            lret = kret + ':\n' + unret[kret] + '\n'
-                            tret += lret + '\n'
-                        ret = tret
-                    elif not argCheck:
-                        ret = '亲，命令很危险, 你这样子老大会不开森!'
-                else:
-                    ret = '没有检测到将要执行的命令，请重新输入！'
+        if tgt:
+            if arg:
+                argCmd = arg.split()[0]
+                argCheck = argCmd not in dangerCmd
+                if argCheck:
+                    sapi = SaltAPI(url=settings.SALT_API['url'],username=settings.SALT_API['user'],password=settings.SALT_API['password'])
+                    unret = sapi.remote_execution(tgt,'cmd.run',arg)
+                    for kret in unret.keys():
+                        lret = kret + ':\n' + unret[kret] + '\n'
+                        tret += lret + '\n'
+                    ret = tret
+                elif not argCheck:
+                    ret = '亲，命令很危险, 你这样子老大会不开森！'
             else:
-                ret = '没有检测到目标主机，请重新输入！'
+                ret = '没有输入命令，请重新输入！'
+        else:
+            ret = '没有指定目标主机，请重新输入！'
          
     return render_to_response('salt_remote_execution.html',
            {'ret': ret},context_instance=RequestContext(request)) 
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python26 manage.py runserver 0.0.0.0:18080
